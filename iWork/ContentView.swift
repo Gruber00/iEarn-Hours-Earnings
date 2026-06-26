4bac9b0a94e98dea5065c464258dcdc1a95718e2
Felix Leonhard Gruber <dev@leonhardgruber.com>
2026-06-26 23:48:06 +0200

    Initial Commit

---
+++ iWork/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iWork
+//
+//  Created by Felix on 26.06.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iWork/iWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWorkApp.swift
+//  iWork
+//
+//  Created by Felix on 26.06.26.
+//
+
+import SwiftUI
+
+@main
+struct iWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

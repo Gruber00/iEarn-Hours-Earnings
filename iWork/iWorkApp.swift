e6cef58daa30299393dab05fa078da572a23579c
Gruber <dev@leonhardgruber.com>
2026-06-27 00:37:28 +0200

    V1

--- iWork/iWorkApp.swift
+++ iWork/iWorkApp.swift
@@ -5,6 +5,7 @@
 //  Created by Felix on 26.06.26.
 //
 
+import SwiftData
 import SwiftUI
 
 @main
@@ -13,5 +14,6 @@ struct iWorkApp: App {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: [WorkEntry.self, SettingsModel.self])
     }
 }
